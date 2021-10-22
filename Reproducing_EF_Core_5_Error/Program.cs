53afb93c0c3b20fc28f13167176ace847ef5dbcd
Даниил Федоров <FedorovDS@msk.bcs.ru>
2021-10-22 11:22:58 +0300

    Initial commit

---
+++ Reproducing_EF_Core_5_Error/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reproducing_EF_Core_5_Error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
